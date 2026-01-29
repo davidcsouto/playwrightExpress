fb727edcbd12406ccb3d19847445d8e6ef3a9686
David Souto <csouto.david@gmail.com>
2026-01-28 23:06:06 -0300

    Removendo teste de validação da home e adicionando execução headless como true

--- tests/home.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('webapp deve estar online', async ({page}) => {
-
-    await page.goto('/')
-    await expect(page).toHaveTitle('Gerencie suas tarefas com Mark L')
-})
